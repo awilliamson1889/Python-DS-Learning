bfa2408a5517352edc466f88ed115866bed14c26
awilliamson1889 <74731998+awilliamson1889@users.noreply.github.com>
2021-02-15 03:55:07 +0300

    Revert "Update test_reader.py"
    This reverts commit 82569c1dd3acb84bcb5615545420f62a2d7ba3b3.

--- src/test_reader.py
+++ src/test_reader.py
@@ -20,11 +20,9 @@ my_dataset1 = DSReader(my_data_test1)
 my_dataset1.to_lower()
 my_dataset1.remove_digits()
 my_dataset1.remove_punctuation_marks()
-my_dataset1.remove_stopwords()
 my_dataset1.remove_duplicates()
-
-# 5 steps to clean dataset
-# to_lower() -> remove_digits() -> remove_punctuation_marks() -> remove_stopwords() -> remove_duplicates()
+my_dataset1.remove_stopwords()
+my_dataset1.remove_stopwords()
 
 print(my_dataset1.dataset)
 
