7f3a0494ebba5f88c82d503fd43a39f0f533bc7d
awilliamson1889 <74731998+awilliamson1889@users.noreply.github.com>
2021-02-25 14:32:46 +0300

    Update class methods
    Updates:
    updated class methods,
    added function logging.

--- src/reader.py
+++ src/reader.py
@@ -1,8 +1,9 @@
 """This module does to work with dataset"""
+import logging
 import string
 import math
 import pandas as pd
-import numpy as np
+import numpy as np  # pylint: disable=W0611
 import nltk
 
 
@@ -39,10 +40,20 @@ class DSReader:
         return tuple of train and test data
     """
 
+    LOG_FORMAT = "%(levelname)s %(asctime)s, %(funcName)s - %(message)s"
+    logging.basicConfig(filename='w.log',
+                        level=logging.DEBUG,
+                        format=LOG_FORMAT,
+                        filemode='w')
+
     def __init__(self, patch):
         self.patch = patch
         self.dataset = DSReader.load_dataset(self.patch)
-        self.dictionary = []
+        self.dictionary = 'Dictionary is empty. Use make_dictionary() method'
+
+        logging.debug(f'INITIAL\npath: {self.patch},'
+                      f'\ndataset: {self.dataset},'
+                      f'\ndictionary: {self.dictionary}')
 
     @staticmethod
     def load_dataset(path):
@@ -53,58 +64,77 @@ class DSReader:
             full path to the dataset
         """
 
+        logging.info('Starting the load_dataset method')
+        logging.debug(f'INPUT DATA:\nDataset path: {path}')
+
         data_frame_csv = pd.read_csv(path)
+
+        logging.info('Dataset successful loaded!')
+        logging.debug(f'Dataset:\n{data_frame_csv} ')
         return data_frame_csv
 
     def remove_digits(self):
-        """Method for remove digits from a dataset
-        """
+        """Method for remove digits from a dataset"""
 
         def no_digits(inp_str):
             inp_str = str(inp_str)
             no_digits_str = ''
             for char in inp_str:
-                if char not in string.digits:
+                if char not in string.digits and char in string.ascii_letters:
                     no_digits_str = no_digits_str + char
                 else:
                     no_digits_str = no_digits_str + ' '
             return no_digits_str
 
+        logging.info('Starting the remove_digits method')
+
         self.dataset['email'] = self.dataset['email'].map(no_digits)
+
+        logging.info('Digits successful removed from dataset!')
+        logging.debug(f'Dataset:\n{self.dataset} ')
         return self.dataset
 
     def to_lower(self):
-        """Method for converting a dataset to lowercase
-        """
+        """Method for converting a dataset to lowercase"""
+
+        logging.info('Starting the to_lower method')
 
         self.dataset['email'] = self.dataset['email'].str.lower()
+
+        logging.info('The dataset is successfully converted to lowercase!')
+        logging.debug(f'Dataset:\n{self.dataset} ')
         return self.dataset
 
     def remove_punctuation_marks(self):
-        """Method for remove punctuation marks from a dataset
-        """
+        """Method for remove punctuation marks from a dataset"""
+
+        punctuation_string = string.punctuation + '—–'
 
         def no_punctuation(inp_str):
             inp_str = str(inp_str)
             no_punctuation_str = ""
             for char in inp_str:
-                if char not in string.punctuation:
+                if char not in punctuation_string and char in string.ascii_letters:
                     no_punctuation_str = no_punctuation_str + char
                 else:
                     no_punctuation_str = no_punctuation_str + ' '
             return no_punctuation_str
 
+        logging.info('Starting the remove_punctuation_marks method')
+
         self.dataset['email'] = self.dataset['email'].map(no_punctuation)
+
+        logging.info('Punctuation marks successful removed from dataset!')
+        logging.debug(f'Dataset:\n{self.dataset} ')
         return self.dataset
 
     def remove_stopwords(self):
-        """Method for remove stopwords from a dataset
-        """
+        """Method for remove stopwords from a dataset"""
 
         stop_words = tuple(nltk.corpus.stopwords.words('english'))
 
         def no_stopwords(inp_str):
-            words = nltk.word_tokenize(inp_str)
+            words = nltk.word_tokenize(str(inp_str))
             new_list_str = ' '.join(words)
             words = (new_list_str.lower()).split()
 
@@ -115,43 +145,50 @@ class DSReader:
             new_str = ' '.join(without_short_words)
             return new_str
 
+        logging.info('Starting the remove_stopwords method')
+
         self.dataset['email'] = self.dataset['email'].map(no_stopwords)
+
+        logging.info('Stopwords successful removed from dataset!')
+        logging.debug(f'Dataset:\n{self.dataset} ')
         return self.dataset
 
     def remove_duplicates(self):
-        """Method for remove duplicates from a dataset
-        """
+        """Method for remove duplicates from a dataset"""
 
+        logging.info('Starting the remove_duplicates method!')
         self.dataset = pd.DataFrame.drop_duplicates(self.dataset)
+
+        logging.info('Duplicates successful removed from dataset!')
+        logging.debug(f'Dataset:\n{self.dataset} ')
         return self.dataset
 
     def make_dictionary(self):
-        """Method for creating a list of all words in a dataset
-        """
+        """Method for creating a list of all words in a dataset"""
 
+        logging.info('Starting the make_dictionary method!')
+
+        list_words = []
         email_index = self.dataset.index
 
         for i in email_index:
-            inp_str = str(self.dataset['email'][i])
-            email_text = inp_str.split()
-            self.dictionary.extend(email_text)  # mb append
+            list_words.extend(self.dataset['email'][i].split())
+
+        self.dictionary = list(set(list_words))
+
+        logging.info('Dictionary successful created!')
 
         return self.dictionary
 
     def vectorize(self):
-        """Method for creating lists of emails and labels
-        """
+        """Method for creating lists of emails and labels"""
 
-        email_index = self.dataset.index
-        email_list = []
+        logging.info('Starting the vectorize method!')
 
-        for i in email_index:
-            inp_str = str(self.dataset['email'][i])
-            email_text = inp_str.split()
-            email_list.append(email_text)
+        emails = pd.DataFrame(self.dataset['email']).values
+        emails_labels = pd.Series(self.dataset['label']).values
 
-        emails = np.array(email_list, dtype=object)
-        emails_labels = np.array(self.dataset['label'])
+        logging.info('Dataset successful splitted(vectorize)!')
 
         return emails, emails_labels
 
@@ -166,12 +203,15 @@ class DSReader:
             a list consisting of email labels
         """
 
+        logging.info('Starting the split_train_and_test method!')
+
         train_count = math.floor(list_email.size * 0.75)
 
-        train_labels = np.array(list_label[:train_count])
-        test_labels = np.array(list_label[train_count:])
+        train_labels = list_label[:train_count]
+        test_labels = list_label[train_count:]
 
-        train_emails = np.array(list_email[:train_count])
-        test_emails = np.array(list_email[train_count:])
+        train_emails = list_email[:train_count]
+        test_emails = list_email[train_count:]
 
+        logging.info('Dataset successful splitted!')
         return train_emails, train_labels, test_emails, test_labels
