f4351f14496e05b2e54d798b6ed46bd3e39e0e21
awilliamson1889 <74731998+awilliamson1889@users.noreply.github.com>
2021-03-15 15:18:30 +0300

    Model test

--- src/email_clf_model.py
+++ src/email_clf_model.py
@@ -1,14 +1,17 @@
+import os
 import pandas as pd
 from src.reader import DSReader
 from sklearn.pipeline import Pipeline
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.linear_model import SGDClassifier
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
+from sklearn.feature_selection import SelectKBest, chi2
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfTransformer
 
-
-emails_data = DSReader('C:/Users/Masquerade/Downloads/emails.csv')
+dataset_path = os.path.abspath("../tests/datasets/emails.csv")
+emails_data = DSReader(dataset_path)
 
 emails_data.to_lower()
 emails_data.remove_digits()
@@ -18,52 +21,57 @@ emails_data.remove_stopwords()
 
 X, y = emails_data.vectorize()
 
-# X_train, y_train, X_test, y_test = emails_data.split_train_and_test(X, y)
-X_train, X_test, y_train, y_test = train_test_split(X, y)
+size = [0.15, 0.25, 0.5, 0.75, 0.95]
 
-pipeline_MultinomialNB = Pipeline([('vect', CountVectorizer()),
-                                   ('tfidf', TfidfTransformer()),
-                                   ('clf', MultinomialNB()),])
+for s in size:
+    print('TEST SIZE! TEST SIZE! TEST SIZE! = ', s)
+    # X_train, y_train, X_test, y_test = emails_data.split_train_and_test(X, y)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=s)
 
-pipeline_SGDClassifier = Pipeline([('vect', CountVectorizer()),
-                                   ('tfidf', TfidfTransformer()),
-                                   ('clf', SGDClassifier(loss='hinge', penalty='l2',
-                                                         alpha=1e-3, random_state=42)),])
+    pipeline_MultinomialNB1 = Pipeline([('vect', CountVectorizer()),
+                                        ('clf', MultinomialNB())])
 
-pipeline_MultinomialNB.fit(X_train.ravel(), y_train)
-pipeline_SGDClassifier.fit(X_train.ravel(), y_train)
+    pipeline_MultinomialNB2 = Pipeline([('vect', CountVectorizer()),
+                                        ('tfidf', TfidfTransformer()),
+                                        ('clf', MultinomialNB())])
 
-predicted_MultinomialNB = pipeline_MultinomialNB.predict(X_test.ravel())
-predicted_SGDClassifier = pipeline_SGDClassifier.predict(X_test.ravel())
+    pipeline_MultinomialNB3 = Pipeline([('vect', CountVectorizer()),
+                                        ('chi', SelectKBest(chi2, k=1200)),
+                                        ('clf', MultinomialNB())])
 
-print(predicted_MultinomialNB)
-print(predicted_SGDClassifier)
+    pipeline_SGDClassifier1 = Pipeline([('vect', CountVectorizer()),
+                                        ('clf', SGDClassifier(loss='hinge', penalty='l2',
+                                                              alpha=1e-3, random_state=42))])
 
-print("Classification rate for MultinomialNB:", pipeline_MultinomialNB.score(X_test.ravel(), y_test))
-print("Classification rate for SGDClassifier:", pipeline_SGDClassifier.score(X_test.ravel(), y_test))
+    pipeline_SGDClassifier2 = Pipeline([('vect', CountVectorizer()),
+                                        ('tfidf', TfidfTransformer()),
+                                        ('clf', SGDClassifier(loss='hinge', penalty='l2',
+                                                              alpha=1e-3, random_state=42))])
 
-my_test_data = [["Hello you win 4000$, please click here !!! --->"],
-                ["Hi Mark. Can you help me please!!!!"],
-                ["hy  hy h@!#12ew #@!123k !!!3 kweoq ck ---wjo 634 oko qd"]]
+    pipeline_SGDClassifier3 = Pipeline([('vect', CountVectorizer()),
+                                        ('chi', SelectKBest(chi2, k=1200)),
+                                        ('clf', SGDClassifier(loss='hinge', penalty='l2',
+                                                              alpha=1e-3, random_state=42))])
 
-my_test_data_clean = [["hello you win please click here"],
-                      ["mark can you help please"],
-                      ["hew kweoq wjo oko"]]
+    pipeline_RandomForestClassifier1 = Pipeline([('vect', CountVectorizer()),
+                                                 ('clf', RandomForestClassifier())])
 
+    pipeline_RandomForestClassifier2 = Pipeline([('vect', CountVectorizer()),
+                                                 ('tfidf', TfidfTransformer()),
+                                                 ('clf', RandomForestClassifier())])
 
-for email in my_test_data:
-    predicted_test_MultinomialNB = pipeline_MultinomialNB.predict(email)
-    predicted_test_SGDClassifier = pipeline_SGDClassifier.predict(email)
+    pipelines = [pipeline_MultinomialNB1, pipeline_MultinomialNB2, pipeline_MultinomialNB3,
+                 pipeline_SGDClassifier1, pipeline_SGDClassifier2, pipeline_SGDClassifier3,
+                 pipeline_RandomForestClassifier1, pipeline_RandomForestClassifier2]
 
-    print(email)
-    print("predicted MultinomialNB: ", predicted_test_MultinomialNB)
-    print("predicted SGDClassifier: ", predicted_test_SGDClassifier)
+    for pipeline in pipelines:
+        pipeline.fit(X_train.ravel(), y_train)
 
-for email in my_test_data_clean:
-    predicted_test_MultinomialNB = pipeline_MultinomialNB.predict(email)
-    predicted_test_SGDClassifier = pipeline_SGDClassifier.predict(email)
-
-    print(email)
-    print("predicted MultinomialNB: ", predicted_test_MultinomialNB)
-    print("predicted SGDClassifier: ", predicted_test_SGDClassifier)
-    
+    print("Classification rate for MultinomialNB1:", pipeline_MultinomialNB1.score(X_test.ravel(), y_test))
+    print("Classification rate for MultinomialNB2:", pipeline_MultinomialNB2.score(X_test.ravel(), y_test))
+    print("Classification rate for MultinomialNB3:", pipeline_MultinomialNB3.score(X_test.ravel(), y_test))
+    print("Classification rate for SGDClassifier1:", pipeline_SGDClassifier1.score(X_test.ravel(), y_test))
+    print("Classification rate for SGDClassifier2:", pipeline_SGDClassifier2.score(X_test.ravel(), y_test))
+    print("Classification rate for SGDClassifier3:", pipeline_SGDClassifier3.score(X_test.ravel(), y_test))
+    print("Classification rate for RandomForestClassifier1:", pipeline_RandomForestClassifier1.score(X_test.ravel(), y_test))
+    print("Classification rate for RandomForestClassifier2:", pipeline_RandomForestClassifier2.score(X_test.ravel(), y_test))
